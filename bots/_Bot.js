0d4a44509677d98d17ebcc4df83641a1371113c5
Luphia <luphia@outlook.com>
2017-07-27 19:09:28 +0800

    Update: basic SIMPLE template

---
+++ bots/_Bot.js
@@ -0,0 +1,29 @@
+var Bots = [];
+
+var Bot = class {
+	constructor() {
+		Bots.push(this);
+	}
+	init({config, db, logger, i18n}) {
+		this.config = config;
+		this.db = db;
+		this.logger = logger;
+		this.i18n = i18n;
+		return Promise.resolve(this);
+	}
+	start() {
+		return Promise.resolve(true);
+	}
+	ready() {
+		return Promise.resolve(true);
+	}
+	getBot(name) {
+		var condition = new RegExp('^' + name + '$', 'i');
+		var bot = Bots.find(function (b) { return condition.test(b.name) });
+		return Promise.resolve(bot);
+	}
+};
+
+
+
+module.exports = Bot;
